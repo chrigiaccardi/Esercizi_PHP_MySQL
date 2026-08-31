e05161991a933247a320afe11bfbdb3c14f1598e
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-08-31 14:22:05 +0200

    Connessione Al server

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php 
-/* Metodo Procedurale*/
-$connessione = mysqli_connect("127.0.0.1", 'root', '', "Tutorial_MySQL");
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$database = "tutorial_mysql";
 
-$connessioneOop = new mysqli()
+$connessione = new mysqli($host, $user, $password, $database);
 
+if($connessione === false){
+    die("Errore di Connessione:" . $connessione->connect_error);
+};
 
+echo "Connessione avvenuta con successo:" . $connessione->host_info;
+
+$connessione->close();
 ?>
