37ea3f9def696f6aceab96ef62c60fd0b239a0b9
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-09-03 14:12:26 +0200

    Creazione dinamica tabella e funziona JS

--- esercizio_1.php
+++ esercizio_1.php
@@ -7,23 +7,7 @@
 </head>
 <body>
     <div id="tabella-container"></div>
-    <table>
-        <thead>
-            <button id="nuova-riga">Inserisci Nuova Persona</button>
-            <tr>
-                <td>ID</td>
-                <td>NOME</td>
-                <td>COGNOME</td>
-                <td>EMAIL</td>
-                <td>
-                </td>
-            </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
-
+    
     <script>
         let persone;
 
@@ -36,11 +20,55 @@
         .then(response => response.json())
         .then(data => {
             persone = data;
-            console.log('Dati Ricevuti: ', data)
+            console.log('Dati Ricevuti: ', data);
+            // Creiamo la tabella dinamicamente
+            let tabella = `
+                <table>
+                    <thead>
+                        <button id="nuova-riga">Inserisci Nuova Persona</button>
+                        <tr>
+                            <td>ID</td>
+                            <td>NOME</td>
+                            <td>COGNOME</td>
+                            <td>EMAIL</td>
+                            <td>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${generaRighe(data)}
+                    </tbody>
+                </table>
+            `
         })
         .catch((error) => {
             console.error("Errore: " , error);
         });
+
+        function generaRighe(persone){
+            let righe = '';
+            // Creiamo un foreach dove per ogni persona creiamo dinamicamente una riga html
+            persone.foreach(persona => {
+                // I due bottoni per ogni riga servono per modificare e eliminare la riga. data-val ci serve per capire l'id della persona selezionata
+                let riga = `
+                    <tr>
+                        <td>${persona.id}</td>
+                        <td>${persona.nome}</td>
+                        <td>${persona.cognome}</td>
+                        <td>${persona.email}</td>
+                        <td>
+                            <button class="modifica-persona" data-val="${persona.id}"></button>
+                            <button class="elimina-persona" data-val="${persona.id}></button>
+                            
+                        </td>
+                    </tr>
+                `;
+                // Ogni riga viene aggiunta alla stringa righe: sono stringhe "html" che poi verranno ritornate alla tabella
+                righe += riga;
+            });
+            // Ritorniamo le righe che verranno inserite all'interno della tabella
+            return righe;
+        }
     </script>
 
 </body>
--- php/select.php
+++ php/select.php
@@ -9,15 +9,19 @@ if($result = $connessione->query($sql)){
     if ($result->num_rows > 0) {
         // MYSQLI_ASSOC -> specifica che vogliamo un array associativo per il risultato che arriva
         while($row = $result->fetch_array(MYSQLI_ASSOC)){
-            $tmp; // $tmp sta per un dato temporaneo
+            // $tmp = temporaneo: dati temporanei e li associamo ai valori del record
+            $tmp;
             $tmp['id'] = $row['id'];
             $tmp['nome'] = $row['nome'];
             $tmp['cognome'] = $row['cognome'];
             $tmp['email'] = $row['email'];
+            // Il dato temporaneo viene pushato all'interno dell'array data.
             array_push($data, $tmp);
         }
+        // Trasforma $data in json e lo manda indietro al frontend
         echo json_encode($data);
     }else {
+        // Trasforma data in json e rimanda l'array vuoto
         echo json_encode($data);
         echo "Non ci sono righe disponibili";
     }
